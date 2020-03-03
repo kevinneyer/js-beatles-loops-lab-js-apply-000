2d5dbdd51fdeb2f017da39dc85acff7d4a8c45ac
Kevin Neyer <kevinneyer@gmail.com>
2020-03-03 15:49:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(){
+  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+  var empty = []
+  
+   for ( let i=0; i<4; i++){empty.push(`${musicians[i]} plays ${instruments[i]}`)}
+   return empty
+}
+
+var facts =[ "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"]
+  
+var lenFacts= ["He was the last Beatle to learn to drive!!!",
+  "He was never a vegetarian!!!",
+  "He was a choir boy and boy scout!!!",
+  "He hated the sound of his own voice!!!"]
+
+
+function johnLennonFacts(facts){
+  while(facts.length < 4){return lenFacts}
+  
+}
